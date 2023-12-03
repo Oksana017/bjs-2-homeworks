ac70384b407fbb7e0017b6f8b0d7401604593d1b
Oksana <oksana017m@mail.com>
2023-12-03 22:19:50 +0300

    Исправлено задание к лекции 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -10,12 +10,6 @@ function compareArrays(arr1, arr2) {
 	});
 }
 
-console.log(compareArrays([8, 9], [6])) // false, разные значения
-console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])) // false, разные значения
-console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])) // false, разные значения
-console.log(compareArrays([1, 2, 3], [2, 3, 1])) // false, разные индексы, хотя и одинаковые значения
-console.log(compareArrays([8, 1, 2], [8, 1, 2])) // true
-
 // Задача 2
 function getUsersNamesInAgeRange(users, gender) {
 	const filterUsers = users.filter(user => user.gender === gender);
@@ -112,9 +106,4 @@ const people = [{
 		age: 19,
 		gender: "мужской"
 	},
-
 ]
-console.log(getUsersNamesInAgeRange(people, "мужской")); // 32
-console.log(getUsersNamesInAgeRange(people, "женский")); // 27.4
-console.log(getUsersNamesInAgeRange([], "женский")); // 0
-console.log(getUsersNamesInAgeRange(people, "инопланетянин")); // 0
